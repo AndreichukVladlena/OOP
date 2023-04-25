e8fe1890a3706b698dff20a4a44f97220d913639
vlanuka <andreychuk04@mail.ru>
2023-04-25 17:16:36 +0300

    Constants file added

---
+++ constants.kt
@@ -0,0 +1,12 @@
+import java.time.LocalDate
+val aimList = listOf("weight loss", "weight maintenance", "weight gain")
+val maleList = listOf("male", "female")
+
+val yearRangeList = listOf(1900.0F, LocalDate.now().year.toFloat())
+val monthRangeList = listOf(1.0F, 12.0F)
+val dayRangeList = listOf(1.0F, 31.0F)
+val weightRangeList = listOf(10.0F, 250.0F)
+val heightRangeList = listOf(50.0F, 250.0F)
+val waterAmountRangeList = listOf(0.0F, 10.0F)
+val caloriesRangeList = listOf(5.0F, 10000.0F)
+val amountRangeList = listOf(1.0F, 300.0F)
